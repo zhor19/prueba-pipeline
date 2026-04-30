80989e876f212bf8ca7174477febb1461076a825
ISSEC\oscar.gordillo <oscar.gordillo@telespazio.com>
2026-04-30 13:51:34 +0100

    hora

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,26 @@
-import './App.css';
-import useState from "react";
+import { useState, useEffect } from "react";
+
 function App() {
-  const [hora] = useState(new Date());
+  const [hora, setHora] = useState(new Date());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setHora(new Date());
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
 
   return (
     <main className="container mx-auto mt-10 bg-amber-100 p-10">
       <h1>Actividad DUAL 4 - CI/CD</h1>
 
-      <div className=" border-2 mx-auto flex max-w-sm items-center gap-x-4 rounded-xl bg-white p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10">
+      <div className="border-2 mx-auto flex max-w-sm items-center gap-x-4 rounded-xl bg-white p-6 shadow-lg">
         <div>
-          <div className="text-xl font-medium text-black dark:text-white">Despliegue</div>
-          <p className  ="text-gray-500 dark:text-gray-400">{hora.toLocaleDateString()}</p>
+          <div className="text-xl font-medium">Despliegue</div>
+          <p>{hora.toLocaleDateString()}</p>
         </div>
       </div>
     </main>
   );
 }
-export default App;
